6fa1f14295aa699c83e27fb96d061c0e6a1856ea
Stephane Harushakamwe <steha@MacBook-Air-som-tillhor-Stephane.local>
2024-11-06 16:11:56 +0100

    First version of io task

--- TDDE18_Lab_1/io.cpp
+++ TDDE18_Lab_1/io.cpp
@@ -1,1 +1,22 @@
+//io.cpp
+#include <iostream>
 
+using namespace std;
+
+int main(){
+
+    //Printout 1
+    int n;
+    cout << "Enter one integer: " << flush;
+    cin >> n;
+    cout << "You entered the number: " << n << endl;
+
+    //Printout 2
+    int x1, x2, x3, x4;
+    cout << "Enter four integer: " << flush;
+    cin >> x1 >> x2 >> x3 >> x4;;
+    cout << "You entered the numbers: " << x1 << x2 << x3 << x4 << endl;
+    
+    
+    return 0;
+}
